e75dc84df0140bf2d65debed35cb57f40107d243
majo <ritz1229@icloud.com>
2022-02-26 21:02:37 +0900

    [AC] abc241 abc241_a

---
+++ atcoder.jp/abc241/abc241_a/Main.kt
@@ -0,0 +1,26 @@
+import kotlin.math.abs
+
+fun main() {
+    val line = readInts()
+    var temp = 0
+    
+    repeat (3) {
+        temp = line[temp]
+    }
+    
+    println(temp)
+ }
+
+private fun readLn() = readLine()!!
+private fun readInt() = readLn().toInt()
+private fun readStrings() = readLn().split(" ")
+private fun readInts() = readStrings().map { it.toInt() }
+private fun readLineFaster(n: Int): List<String> {
+    val reader = System.`in`.bufferedReader()
+    val list = mutableListOf<String>()
+    
+    for (i in 1..n) list.add(reader.readLine()!!)
+    
+    val answer: List<String> = list
+    return answer
+}
